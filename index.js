54b9b33b2762e824619927750198788f1ea6d8af
schpo592 <schpo592@student.otago.ac.nz>
2022-08-04 13:16:06 +1200

    got the bird cards working

--- index.js
+++ index.js
@@ -37,20 +37,34 @@ async function getJSON() {
     console.log(jsonData[0].primary_name);//WORKS
     console.log(jsonData.length);//68
 
-    birdCards(0);
+    //firstBirdCard(0);
+    for(let i = 0; i < jsonData.length; i++){
+        birdCards(i);
+        console.log(i);
+    }
+    
 
 }
 
 //function to make bird cards
 async function birdCards(count){
 
+    let main = document.getElementById('myMain');
+    let card = document.createElement('div');
+    card.setAttribute('class', 'birdCard');
+
+    const colours = [['Not Threated', '#02a028'], ['Naturally Uncommon', '#649a31'],
+        ['Relict', '#99cb68'], ['Recovering', '#fecc33'], ['Declining', 'f#e9a01'], 
+        [ 'Nationally Increasing', '#c26967'], ['Nationally Vulnerable', '#9b0000'],
+        [ 'Nationally Endangered', '#660032'], ['Nationally Critical', '#320033'],
+        [ 'Extinct', 'black'], ['Data deficient', 'black']];
+
     let data = jsonData;
     let bird = jsonData[count];
     //photo
     let photo = document.createElement('img'); //create img
     photo.setAttribute('class', 'birdPhoto');
-    let card = document.getElementById('bird1');
-    
+
     photo.src = bird.photo.source;
     console.log(photo.src); 
     card.appendChild(photo);//works
@@ -66,7 +80,7 @@ async function birdCards(count){
     topInfo.appendChild(title);
 
     let credits = document.createElement('p');
-    credits.textContent = 'Photo by ' + bird.credit;
+    credits.textContent = 'Photo by ' + bird.photo.credit;
     topInfo.appendChild(credits);
 
     card.appendChild(topInfo);//worked
@@ -121,27 +135,213 @@ async function birdCards(count){
     staName.textContent = bird.status;
     grid.appendChild(staName);
 
-/*
+
     //length
-    let sciBold = document.createElement('p');
-    sciBold.setAttribute('id', 'bold');
-    sciBold.textContent = 'Scientific name';
-    grid.appendChild(sciBold);
-    let sciName = document.createElement('p');
-    sciName.textContent = bird.scientific_name;
-    grid.appendChild(sciName);
+    let lenBold = document.createElement('p');
+    lenBold.setAttribute('id', 'bold');
+    lenBold.textContent = 'Length';
+    grid.appendChild(lenBold);
+    let lenName = document.createElement('p');
+    lenName.textContent = bird.size.length.value + ' ' + bird.size.length.units;
+    grid.appendChild(lenName);
 
     //weight
-    let sciBold = document.createElement('p');
-    sciBold.setAttribute('id', 'bold');
-    sciBold.textContent = 'Scientific name';
-    grid.appendChild(sciBold);
-    let sciName = document.createElement('p');
-    sciName.textContent = bird.scientific_name;
-    grid.appendChild(sciName);
-    */
+    let weiBold = document.createElement('p');
+    weiBold.setAttribute('id', 'bold');
+    weiBold.textContent = 'Weight';
+    grid.appendChild(weiBold);
+    let weiName = document.createElement('p');
+    weiName.textContent = bird.size.weight.value + ' ' + bird.size.weight.units;
+    grid.appendChild(weiName);
+    
 
     bottomInfo.appendChild(grid);
     card.appendChild(bottomInfo);
 
+
+
+    //need to add coloured circle
+
+    //border for circle
+    let border = document.createElement('span');
+    border.setAttribute('class', 'circleBorder');
+    card.appendChild(border);
+
+    //think its a span i need to add with alil border?
+    let circle = document.createElement('span');
+    circle.setAttribute('class', 'circle');
+    //circle.setAttribute('id', 'circleColour');
+    
+
+    for(let i = 0; i < colours.length ; i++){
+        let status = bird.status;
+        let status2 = colours[i][0];
+        //let result = (status).localCompare(status2);
+        if(status === status2){
+            //circle.setAttribute('background-colour', colours[i][1]);
+            //let c = document.getElementById('circle');
+            let col = colours[i][1];
+            circle.style.backgroundColor = col;
+            console.log(col);
+        }
+    }
+    card.appendChild(circle);
+
+    main.appendChild(card);
+
 }
+
+//can prob delete now
+async function firstBirdCard(count){
+
+    const colours = [['Not Threated', '#02a028'], ['Naturally Uncommon', '#649a31'],
+        ['Relict', '#99cb68'], ['Recovering', '#fecc33'], ['Declining', 'f#e9a01'], 
+        [ 'Nationally Increasing', '#c26967'], ['Nationally Vulnerable', '#9b0000'],
+        [ 'Nationally Endangered', '#660032'], ['Nationally Critical', '#320033'],
+        [ 'Extinct', 'black'], ['Data deficient', 'black']];
+
+    let data = jsonData;
+    let bird = jsonData[count];
+    //photo
+    let photo = document.createElement('img'); //create img
+    photo.setAttribute('class', 'birdPhoto');
+
+    //let card = document.createElement('div');
+    //card.setAttribute('id', 'bird1');
+    let card = document.getElementById('bird1');
+    //let main = document.getElementsByClassName('main');
+    //main.appendChild(card);
+    
+    photo.src = bird.photo.source;
+    console.log(photo.src); 
+    card.appendChild(photo);//works
+
+
+    //information
+    let maori = jsonData[count].primary_name;
+    let topInfo = document.createElement('div');
+    topInfo.setAttribute('class', 'topInfo');
+    let title = document.createElement('h2');
+    title.textContent = maori;
+    console.log(title);
+    topInfo.appendChild(title);
+
+    let credits = document.createElement('p');
+    credits.textContent = 'Photo by ' + bird.photo.credit;
+    topInfo.appendChild(credits);
+
+    card.appendChild(topInfo);//worked
+
+    //bottom info
+    let bottomInfo = document.createElement('div');
+    bottomInfo.setAttribute('class', 'bottomInfo');
+    let name = bird.english_name;
+    let engName = document.createElement('h2');
+    engName.textContent = name; // could i make it = bird.english_name; ???
+    bottomInfo.appendChild(engName);
+
+    //grid of other info
+
+    //scientific name
+    let grid = document.createElement('div');
+    grid.setAttribute('class', 'gridInfo');
+    let sciBold = document.createElement('p');
+    sciBold.setAttribute('id', 'bold');
+    sciBold.textContent = 'Scientific name';
+    grid.appendChild(sciBold);
+    let sciName = document.createElement('p');
+    sciName.textContent = bird.scientific_name;
+    grid.appendChild(sciName);
+
+    //family
+    let famBold = document.createElement('p');
+    famBold.setAttribute('id', 'bold');
+    famBold.textContent = 'Family';
+    grid.appendChild(famBold);
+    let famName = document.createElement('p');
+    famName.textContent = bird.family;
+    grid.appendChild(famName);
+    
+
+    //order
+    let ordBold = document.createElement('p');
+    ordBold.setAttribute('id', 'bold');
+    ordBold.textContent = 'Order';
+    grid.appendChild(ordBold);
+    let ordName = document.createElement('p');
+    ordName.textContent = bird.order;
+    grid.appendChild(ordName);
+
+
+    //status
+    let staBold = document.createElement('p');
+    staBold.setAttribute('id', 'bold');
+    staBold.textContent = 'Status';
+    grid.appendChild(staBold);
+    let staName = document.createElement('p');
+    staName.textContent = bird.status;
+    grid.appendChild(staName);
+
+
+    //length
+    let lenBold = document.createElement('p');
+    lenBold.setAttribute('id', 'bold');
+    lenBold.textContent = 'Length';
+    grid.appendChild(lenBold);
+    let lenName = document.createElement('p');
+    lenName.textContent = bird.size.length.value + ' ' + bird.size.length.units;
+    grid.appendChild(lenName);
+
+    //weight
+    let weiBold = document.createElement('p');
+    weiBold.setAttribute('id', 'bold');
+    weiBold.textContent = 'Weight';
+    grid.appendChild(weiBold);
+    let weiName = document.createElement('p');
+    weiName.textContent = bird.size.weight.value + ' ' + bird.size.weight.units;
+    grid.appendChild(weiName);
+    
+
+    bottomInfo.appendChild(grid);
+    card.appendChild(bottomInfo);
+
+
+
+    //need to add coloured circle
+
+    //border for circle
+    let border = document.createElement('span');
+    border.setAttribute('class', 'circleBorder');
+    card.appendChild(border);
+
+    //think its a span i need to add with alil border?
+    let circle = document.createElement('span');
+    circle.setAttribute('class', 'circle');
+    //circle.setAttribute('id', 'circleColour');
+    
+
+    for(let i = 0; i < colours.length ; i++){
+        let status = bird.status;
+        let status2 = colours[i][0];
+        //let result = (status).localCompare(status2);
+        if(status === status2){
+            //circle.setAttribute('background-colour', colours[i][1]);
+            //let c = document.getElementById('circle');
+            let col = colours[i][1];
+            circle.style.backgroundColor = col;
+            console.log(col);
+        }
+    }
+    card.appendChild(circle);
+
+}
+
+
+//work on search feature
+
+
+//work on status sort
+
+
+//work on sort by
+

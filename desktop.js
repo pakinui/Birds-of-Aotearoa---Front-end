db0a17dc748b2123c1af63d94e4f74f1c5960ebb
schpo592 <schpo592@student.otago.ac.nz>
2022-08-13 22:28:59 +1200

    page

--- desktop.js
+++ desktop.js
@@ -6,9 +6,6 @@
 
 const birdFile = './data/nzbird.json';
 let jsonData; // inital json file bird array
-console.log('starting');
-getJSON();
-startDesktopWebsite();
 /*
     array of colours and which conservation status they represent
 */
@@ -17,25 +14,7 @@ const colours = [['Not Threatened', '#02a028'], ['Naturally Uncommon', '#649a31'
 ['Nationally Increasing', '#c26967'], ['Nationally Vulnerable', '#9b0000'],
 ['Nationally Endangered', '#660032'], ['Nationally Critical', '#320033'],
 ['Extinct', '#000000'], ['Data Deficient', '#000000']];
-//fetchData();
-
-async function getJSON() {
-    console.log('starting to get JSON');
-    let response = await fetch(birdFile);
-    let data = await response.text();
-    jsonData = JSON.parse(data);
-    //console.log(jsonData[0].primary_name);//WORKS
-    //console.log(jsonData.length);//68
-
-    //firstBirdCard(0);
-    for (let i = 0; i < jsonData.length; i++) {
-        //birdCards(i);
-        printCards(jsonData, i);
-        //console.log(i);
-    }
-
-
-}
+fetchData();
 
 /*
     function to fetch data 
@@ -514,4 +493,34 @@ let today = new Date();
 //+1 to the month as the months are from 0-11
 let dayString = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
 date.textContent = dayString;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
